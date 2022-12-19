fix rotation code

snow_globe/geometry.py
@@ -10,7 +10,8 @@ def rotate_array(arr: npt.ArrayLike, angle: AngleRadians) -> npt.NDArray[np.floa
     arr = np.array(arr, dtype=np.float64)
     rot_matrix = get_clockwise_rotation_matrix_2D(angle)
     # have to invert rotation matrix to preserve format of position array
-    return np.linalg.inv(rot_matrix).dot(arr)
+    # return np.linalg.inv(rot_matrix).dot(arr)
+    return arr @ np.linalg.inv(rot_matrix)
 
 
 def get_clockwise_rotation_matrix_2D(theta: AngleRadians) -> npt.NDArray[np.floating]: